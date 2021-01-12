02261010e44b9e525c15535d464e3e09b0d3f571
Karikaranvetti <Karikaranvetti>
2021-01-12 14:01:08 +0530

    cart and checkout page dynamic data renderd

--- store/views.py
+++ store/views.py
@@ -9,9 +9,29 @@ def store(request):
 	return render(request,'store/Store.html',context)
 
 def cart(request):
-	context={}
+
+	if request.user.is_authenticated:
+		customer = request.user.customer
+		order, created = Order.objects.get_or_create(customer=customer, complete=False)
+		items = order.orderitem_set.all()
+	else:
+		#Create empty cart for now for non-logged in user
+		items = []
+		order = {'get_cart_total':0, 'get_cart_items':0}
+
+	context = {'items':items, 'order':order}
+	 
 	return render(request,'store/Cart.html',context)
 
 def checkout(request):
-	context={}
+	if request.user.is_authenticated:
+		customer = request.user.customer
+		order, created = Order.objects.get_or_create(customer=customer, complete=False)
+		items = order.orderitem_set.all()
+	else:
+		#Create empty cart for now for non-logged in user
+		items = []
+		order = {'get_cart_total':0, 'get_cart_items':0}
+
+	context = {'items':items, 'order':order}
 	return render(request,'store/Checkout.html',context)
