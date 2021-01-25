d0787bda02a66ef624a49fa8f3f338c9624070ee
Bragadeeshan <68198134+Bragadeeshan@users.noreply.github.com>
2021-01-25 20:04:49 +0530

    Update settings.py

--- happyvending/settings.py
+++ happyvending/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '7-u!@y&hou!=^59452k_^r7&xp=hfcp3yqoqk!_o8!-b62g=b-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://happyvending.herokuapp.com/']
 
 
 # Application definition
